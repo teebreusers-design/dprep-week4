fbfe183d6f8232b7e3cd7d296fe0220d630daea4
TessaBreusers <t.e.e.breusers@tilburguniversity.edu>
2026-09-18 11:33:08 +0200

    Add timestemp conversion and laggeddaily metrics

--- src/data_engineering.R
+++ src/data_engineering.R
@@ -1,24 +1,38 @@
 # Load the data
+setwd("C:/Users/tessa/OneDrive/Master Marketing Analytics/dpreptutorial/dprep-week4/data")
 library(tidyverse)
 
-video_view <- read.csv("data/video_view.csv")
-setwd()
+
+video_view <- read_csv("video_view.csv")
+users <- read_csv("users.csv")
+impressions <- read_csv("impressions.csv")
+watch_events <- read_csv("watch_events.csv")
+sessions <- read_csv("sessions.csv")
 
 # Exercise 1
 
-video_features <- video_view %>% mutate(watch_rate_rank = rank(-watch_rate))
+video_features <- video_view %>%
+  mutate(
+    watch_rate_rank = rank(-watch_rate),
+    reach_band = case_when(
+      impressions_n < 20 ~ "Low",
+      impressions_n < 60 ~ "Medium",
+      TRUE ~ "High"
+    ),
+    high_quality = avg_watch_share >= 0.40
+  ) %>%
+  distinct(video_id, .keep_all = TRUE) %>%
+  arrange(watch_rate_rank)
 
-video_features <- video_view %>% mutate(reach_band = cut(impressions_n, breaks = c(0,9,41,230), labels = c("Low", "Medium", "High")))
-
-video_features <- video_view %>% mutate(high_quality = avg_watch_share >= 0.40)
-
-video_features <- video_view %>% distinct(video_id, .keep_all = TRUE)
-
-write.csv(video_features, "temp/video_features.csv")
+write_csv(video_features, "temp/video_features.csv")
 
 # Exercise 2
 
+creator_summary <- video_features %>% group_by(creator_id) %>% summarise(videos_n = n(), impressions_total = sum(impressions_n),
+              watched_total = sum(watched_n), avg_watch_rate = mean(watch_rate),
+              median_watch_seconds = median(total_watch_seconds))
 
+engagement_by_band <- video_features %>% group_by(reach_band) %>% summarise(videos_n = n(), avg_watch_rate = mean(watch_rate))
 
 # Exercise 3
 
